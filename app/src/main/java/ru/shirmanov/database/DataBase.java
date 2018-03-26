2b1bb24d0da08425f94db3b0ccabb9b7f98f6f38
drond <drondiwe@yandex.ru>
2018-03-26 23:23:25 +0300

    Initial commit

---
+++ app/src/main/java/ru/shirmanov/database/DataBase.java
@@ -0,0 +1,21 @@
+package ru.shirmanov.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBase extends SQLiteOpenHelper {
+    public DataBase (Context context) {
+        super(context, "db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table Contact(id integer primary key autoincrement, " + "name text, phone integer);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
